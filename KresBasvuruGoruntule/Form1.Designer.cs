0e533a766bd0db214275c3ed53052bd5468a4147
esraoznaz <91626074+esraoznaz@users.noreply.github.com>
2024-08-12 08:49:49 +0300

    Add project files.

---
+++ KresBasvuruGoruntule/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace KresBasvuruGoruntule
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(3, -1);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(1900, 383);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button1.Location = new System.Drawing.Point(216, 419);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(136, 55);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "FİLTRELE";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(24, 419);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(147, 22);
+			this.textBox1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1924, 535);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Başvuru Görüntüle";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ KresBasvuruGoruntule/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.ComponentModel; 
+using System.Data; 
+using System.Data.SqlClient;
+using System.Drawing; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+using System.Windows.Forms; 
+ 
+namespace KresBasvuruGoruntule 
+{ 
+	public partial class Form1 : Form 
+	{
+
+		//private DataGridView dataGridView1 = new DataGridView();
+		private string connectionString = "Server=LAPTOP-3H9G77VD\\SQLEXPRESS;Database=Kres_Basvuru;Integrated Security=True";
+		public Form1() 
+		{ 
+			InitializeComponent();
+
+			Load += new EventHandler(Form1_Load);
+			Controls.Add(dataGridView1); // Bu satırı kontrol edin ve eklemenin doğru yerde olduğundan emin olun
+			dataGridView1.Dock = DockStyle.Fill;
+		} 
+         
+		private void Form1_Load(object sender, EventArgs e) 
+		{ 
+			LoadData();
+		}
+
+		private void LoadData()
+		{
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string query = @"
+                SELECT 
+                    Cocuk_Bilgisi.Cocuk_Id,
+                    Cocuk_Bilgisi.Cocuk_Adi_Soyadi,
+                    Cocuk_Bilgisi.Cocuk_TC_No,
+                    Cocuk_Bilgisi.Dogum_Tarihi,
+                    Cocuk_Bilgisi.Cocuk_Cinsiyeti,
+                    Cocuk_Bilgisi.Ev_Adresi,
+                    Anne_Bilgisi.Anne_Adi_Soyadi,
+                    Anne_Bilgisi.Anne_TC_No,
+                    Anne_Bilgisi.Anne_Meslegi,
+                    Anne_Bilgisi.Anne_Meslek_Tipi,
+                    Anne_Bilgisi.Anne_Acik_Is_Adresi,
+                    Anne_Bilgisi.Anne_Calisma_Saatleri,
+                    Anne_Bilgisi.Anne_Cep_Telefonu,
+                    Anne_Bilgisi.Anneye_Ulasilamadiginda_Irtibat_No,
+                    Anne_Bilgisi.Anne_Durumu,
+                    Baba_Bilgisi.Baba_Adi_Soyadi,
+                    Baba_Bilgisi.Baba_TC_No,
+                    Baba_Bilgisi.Baba_Meslegi,
+                    Baba_Bilgisi.Baba_Meslek_Tipi,
+                    Baba_Bilgisi.Baba_Acik_Is_Adresi,
+                    Baba_Bilgisi.Baba_Calisma_Saatleri,
+                    Baba_Bilgisi.Baba_Cep_Telefonu,
+                    Baba_Bilgisi.Babaya_Ulasilamadiginda_Irtibat_No,
+                    Baba_Bilgisi.Baba_Durumu,
+                    Bakanin_Bilgisi.Emekli_Personel_Torun,
+                    Bakanin_Bilgisi.Bakan_TC_No,
+                    Bakanin_Bilgisi.Bakan_Telefon_Numarasi,
+                    Basvuranin_Bilgisi.Telefon,
+                    Cocuk_Harici_Bilgi.Cocuk_Kronik_Hastalik,
+                    Cocuk_Harici_Bilgi.Cocuk_Alerji,
+                    Cocuk_Harici_Bilgi.Cocuk_Davranis_Problemi,
+                    Cocuk_Harici_Bilgi.Cocuk_Ozel_Durum
+                FROM 
+                    Cocuk_Bilgisi
+                LEFT JOIN 
+                    Anne_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Anne_Bilgisi.Cocuk_Id
+                LEFT JOIN 
+                    Baba_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Baba_Bilgisi.Cocuk_Id
+                LEFT JOIN 
+                    Bakanin_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Bakanin_Bilgisi.Cocuk_Id
+                LEFT JOIN 
+                    Basvuranin_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Basvuranin_Bilgisi.Cocuk_Id
+                LEFT JOIN 
+                    Cocuk_Harici_Bilgi ON Cocuk_Bilgisi.Cocuk_Id = Cocuk_Harici_Bilgi.Cocuk_Id;";
+
+				SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+				DataTable dataTable = new DataTable();
+				dataAdapter.Fill(dataTable);
+
+				dataGridView1.DataSource = dataTable;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string cocukId = textBox1.Text;
+			LoadFilteredData(cocukId);
+		}
+
+		private void LoadFilteredData(string cocukId)
+		{
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string query = @"
+            SELECT 
+                Cocuk_Bilgisi.Cocuk_Id,
+                Cocuk_Bilgisi.Cocuk_Adi_Soyadi,
+                Cocuk_Bilgisi.Cocuk_TC_No,
+                Cocuk_Bilgisi.Dogum_Tarihi,
+                Cocuk_Bilgisi.Cocuk_Cinsiyeti,
+                Cocuk_Bilgisi.Ev_Adresi,
+                Anne_Bilgisi.Anne_Adi_Soyadi,
+                Anne_Bilgisi.Anne_TC_No,
+                Anne_Bilgisi.Anne_Meslegi,
+                Anne_Bilgisi.Anne_Meslek_Tipi,
+                Anne_Bilgisi.Anne_Acik_Is_Adresi,
+                Anne_Bilgisi.Anne_Calisma_Saatleri,
+                Anne_Bilgisi.Anne_Cep_Telefonu,
+                Anne_Bilgisi.Anneye_Ulasilamadiginda_Irtibat_No,
+                Anne_Bilgisi.Anne_Durumu,
+                Baba_Bilgisi.Baba_Adi_Soyadi,
+                Baba_Bilgisi.Baba_TC_No,
+                Baba_Bilgisi.Baba_Meslegi,
+                Baba_Bilgisi.Baba_Meslek_Tipi,
+                Baba_Bilgisi.Baba_Acik_Is_Adresi,
+                Baba_Bilgisi.Baba_Calisma_Saatleri,
+                Baba_Bilgisi.Baba_Cep_Telefonu,
+                Baba_Bilgisi.Babaya_Ulasilamadiginda_Irtibat_No,
+                Baba_Bilgisi.Baba_Durumu,
+                Bakanin_Bilgisi.Emekli_Personel_Torun,
+                Bakanin_Bilgisi.Bakan_TC_No,
+                Bakanin_Bilgisi.Bakan_Telefon_Numarasi,
+                Basvuranin_Bilgisi.Telefon,
+                Cocuk_Harici_Bilgi.Cocuk_Kronik_Hastalik,
+                Cocuk_Harici_Bilgi.Cocuk_Alerji,
+                Cocuk_Harici_Bilgi.Cocuk_Davranis_Problemi,
+                Cocuk_Harici_Bilgi.Cocuk_Ozel_Durum
+            FROM 
+                Cocuk_Bilgisi
+            LEFT JOIN 
+                Anne_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Anne_Bilgisi.Cocuk_Id
+            LEFT JOIN 
+                Baba_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Baba_Bilgisi.Cocuk_Id
+            LEFT JOIN 
+                Bakanin_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Bakanin_Bilgisi.Cocuk_Id
+            LEFT JOIN 
+                Basvuranin_Bilgisi ON Cocuk_Bilgisi.Cocuk_Id = Basvuranin_Bilgisi.Cocuk_Id
+            LEFT JOIN 
+                Cocuk_Harici_Bilgi ON Cocuk_Bilgisi.Cocuk_Id = Cocuk_Harici_Bilgi.Cocuk_Id
+            WHERE 
+                Cocuk_Bilgisi.Cocuk_Id = @Cocuk_Id";
+
+				SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+				dataAdapter.SelectCommand.Parameters.AddWithValue("@Cocuk_Id", cocukId);
+				DataTable dataTable = new DataTable();
+				dataAdapter.Fill(dataTable);
+
+				dataGridView1.DataSource = dataTable;
+			}
+		}
+	} 
+} 
